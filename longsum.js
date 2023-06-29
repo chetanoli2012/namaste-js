516603d3edf456cf5799306b9c6c3e123f1eda9d
Chetan Oli <chetan-nl@Chetans-MacBook-Pro.local>
2023-06-29 19:15:01 +0545

    Curried function that take 5 arguments in any order

--- longsum.js
+++ longsum.js
@@ -12,3 +12,34 @@ function sum(a) {
 }
 
 sum(1)(2)(3)();
+
+// carete a function that'll accept 5 arguments in any order
+
+const ARGS_LEN = 5;
+
+const newSum = function (...args) {
+  if (args.length === ARGS_LEN) {
+    // calculate sum
+    return args.reduce(function (sum, cur) {
+      return (sum += cur);
+    }, 0);
+  } else {
+    recusiveSum = function (...args2) {
+      args = args.concat(args2);
+      if (args.length === ARGS_LEN) {
+        return args.reduce(function (sum, cur) {
+          return (sum += cur);
+        }, 0);
+      } else {
+        return recusiveSum;
+      }
+    };
+    return recusiveSum;
+  }
+};
+
+console.log(newSum(1)(2)(3)(4)(5)); // 15
+console.log(newSum(1, 2)(3)(4)(5)); // 15
+console.log(newSum(1, 2, 3)(4)(5)); // 15
+console.log(newSum(1, 2, 3, 4)(5)); // 15
+console.log(newSum(1, 2, 3, 4, 5)); // 15
