73d1f78ce5130b16ce6da1f873eca33a40b4af1c
Chetan Oli <chetan-nl@Chetans-MacBook-Pro.local>
2023-06-29 19:27:11 +0545

    Curried function that remembers the previous sum

--- longsum.js
+++ longsum.js
@@ -1,3 +1,5 @@
+console.log("=========== Curried Function that takes n parms ==========");
+
 // sum(1)(2)() -> 3
 // sum(1)(2)(3)() -> 6
 // sum(1)(2)(3)(4)() -> 10 and so on...
@@ -13,7 +15,9 @@ function sum(a) {
 
 sum(1)(2)(3)();
 
-// carete a function that'll accept 5 arguments in any order
+// carate a function that'll accept 5 arguments in any order
+
+console.log("=========== Curried Function that takes 5 parms ==========");
 
 const ARGS_LEN = 5;
 
@@ -43,3 +47,25 @@ console.log(newSum(1, 2)(3)(4)(5)); // 15
 console.log(newSum(1, 2, 3)(4)(5)); // 15
 console.log(newSum(1, 2, 3, 4)(5)); // 15
 console.log(newSum(1, 2, 3, 4, 5)); // 15
+
+// Write a curry fuction that returns the sum of previous values
+
+console.log("===== Curried Function that takes remembers previous sums =====");
+
+const curryFn = function () {
+  let sum = 0;
+  return function (val) {
+    if (!val) {
+      return sum;
+    } else {
+      return (sum += val);
+    }
+  };
+};
+
+const add = curryFn();
+console.log(add(1));
+console.log(add(2));
+console.log(add(3));
+console.log(add(4));
+console.log(add());
